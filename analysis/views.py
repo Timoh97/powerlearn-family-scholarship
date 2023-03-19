9e3749c0c54ebe6895434c9869636637053cdd05
Timoh97 <timohmugendi@gmail.com>
2023-03-20 01:13:48 +0300

    create analysis.views

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+from analysis.models import Order
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url = '/admin_login')
+def dashboard_with_pivot(request):
+    current = request.user
+    if current.is_staff:
+        return render(request, '', {})  #staff_dashboard.html
+
+
+
+def pivot_data(request):
+    dataset = Order.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
+
+
+   
+
