d0ccc51dff4a37a4c6a2ec47959c38250103ceaf
Timoh97 <timohmugendi@gmail.com>
2023-03-19 23:15:37 +0300

    create several apps for modularity

--- logistics/apps.py
+++ logistics/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransportConfig(AppConfig):
+class LogisticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transport'
+    name = 'logistics'
