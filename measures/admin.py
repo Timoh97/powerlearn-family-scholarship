da837acb201b4efc308d267b9a9976925c91d2b6
Timoh97 <timohmugendi@gmail.com>
2023-04-05 14:31:39 +0300

    plp final

--- measures/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Storage,Goods
-# Register your models here.
-admin.site.register(Storage)
-admin.site.register(Goods)
--- warehouse/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Client,Staff,User,Profile
-
-admin.site.register(User)
-admin.site.register(Client)
-admin.site.register(Staff)
-admin.site.register(Profile)
